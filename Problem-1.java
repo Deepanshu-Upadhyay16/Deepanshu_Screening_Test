caadb076759b7df985b3b97f1a43f96c16b5147e
Deepanshu-Upadhyay16 <udeepanshu356@gmail.com>
2025-07-02 00:37:50 +0530

    Add files via upload

---
+++ Problem-1.java
@@ -0,0 +1,50 @@
+// Problem-1.java
+
+import java.util.Scanner;
+
+class Calculator {
+    double a, b;
+    String operation;
+
+    public Calculator(double a, double b, String operation) {
+        this.a = a;
+        this.b = b;
+        this.operation = operation.toLowerCase();
+    }
+
+    public void calculate() {
+        switch (operation) {
+            case "add":
+                System.out.println("Result: " + (a + b));
+                break;
+            case "subtract":
+                System.out.println("Result: " + (a - b));
+                break;
+            case "multiply":
+                System.out.println("Result: " + (a * b));
+                break;
+            case "divide":
+                if (b != 0) {
+                    System.out.println("Result: " + (a / b));
+                } else {
+                    System.out.println("Error: Division by zero");
+                }
+                break;
+            default:
+                System.out.println("Invalid operation");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        double a = sc.nextDouble();
+        System.out.print("Enter b: ");
+        double b = sc.nextDouble();
+        System.out.print("Enter operation (add, subtract, multiply, divide): ");
+        String op = sc.next();
+
+        Calculator calc = new Calculator(a, b, op);
+        calc.calculate();
+    }
+}
